de91520f2ba463df2309b8157e15d42e4d497e0a
Heather Prince <heather.prince1@gmail.com>
2016-11-17 14:02:23 -0500

    Separate mpi version for now, will integrate into euler.cc later once I have mpi running

---
+++ euler_mpi.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include "euler.h"
+#include "model.h"
+#include "grid_mpi.h"
+
+//implementation of Euler that works on a square grid
+Euler::Euler(double dt, const Model &model)
+    : dimen_(model.dimen_()),
+      dimen_y_(model.dimen_y_()),
+      dt_(dt),
+      model_(model) {
+  fx_ = new Grid(dimen_, dimen_y_, 0,0, M_PI, M_PI); // don't actually need xmin, ymin, xmax, ymax, so the last four arguments are unimportant
+}
+
+Euler::~Euler() {
+  delete fx_;
+}
+
+int Euler::Step(double t, Grid &x) {
+  model_.rhs(t, x, *fx_);
+  x.AddGridTimesConst(dt_, *fx_);
+  return 0;
+}
