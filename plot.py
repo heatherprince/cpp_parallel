b1fd213573777e5815204557b0533fa9cddba903
Heather Prince <heather.prince1@gmail.com>
2016-11-18 14:22:21 -0500

    Updated plot function

--- plot.py
+++ plot.py
@@ -1,20 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_serial():
-    nside=128
-    T=np.loadtxt("T_out.txt")
-
-    plt.imshow(T)
-    plt.title("T for nside="+str(nside))
+def plot_T(T_grid, title, saveas):
+    xymax=np.pi
+    plt.imshow(T_grid, origin='lower', extent=[0,xymax,0,xymax])#, cmap='RdBu')
     plt.colorbar()
+    plt.title(title)
+    plt.savefig(saveas)
+    plt.xlabel('x')
+    plt.ylabel('y')
     plt.show()
     plt.close()
+
+
+def plot_serial():
+    nside=[128]
+
+    for ns in nside:
+        T=np.loadtxt("OutputDatafiles/T_out_nside"+str(ns)+"_serial.txt")
+        T_grid=np.transpose(T)
+        plot_T(T_grid, "T calculated in serial for nside="+str(ns), "Plots/T_serial_nside"+str(ns)+".png")
 
 def plot_mpi():
     nside=[128,256]#,512]
     nproc=[1,2,4,8,16]
-    xymax=np.pi
+
 
     for ns in nside:
         T_grid=np.zeros((ns,ns))
@@ -28,21 +38,14 @@ def plot_mpi():
                 T_grid[i_min:i_max, :]=data
 
             T_grid=np.transpose(T_grid) #so that x plots on horizontal axis
+            plot_T(T_grid, "T for "+str(npr)+" processor(s), nside="+str(ns), "Plots/T_"+str(npr)+"processors_nside"+str(ns)+".png")
 
-            plt.imshow(T_grid, origin='lower', extent=[0,xymax,0,xymax])#, cmap='RdBu')
-            plt.colorbar()
-            plt.title("T for "+str(npr)+" processor(s), nside="+str(ns))
-            plt.savefig("Plots/T_"+str(npr)+"processors_nside"+str(ns)+".png")
-            plt.xlabel('x')
-            plt.ylabel('y')
-            plt.show()
-            plt.close()
 
 
 
 
 
 if __name__=="__main__":
-    #plot_serial()
+    plot_serial()
     #plot_omp()
-    plot_mpi()
+    #plot_mpi()
